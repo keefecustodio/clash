932ad56cb92b7b18d8149d8f2835113349d1b21a
Keefe Custodio <keefe,dev@gmail.com>
2021-07-01 23:28:40 -0700

    first commit

---
+++ src/bot.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+// https://www.youtube.com/watch?v=BmKXBVdEV0g
+// import a class directly from discord js library
+const { Client } = require('discord.js')
+const client = new Client()
+
+// 
+client.login()
