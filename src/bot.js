b2fa231061bad342299105bc5a6ba5452014d895
Keefe Custodio <keefecustodio@Keefes-MacBook-Air.local>
2021-07-06 20:06:43 -0700

    closes #4 pickrandom command added

--- src/bot.js
+++ src/bot.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 
 // https://www.youtube.com/watch?v=BmKXBVdEV0g
 // import a class directly from discord js library
-const { Client, Guild } = require('discord.js')
+const { Client, MessageEmbed } = require('discord.js')
 
 const client = new Client()
-const guild = new Guild()
 
 //add prefix
 
@@ -29,18 +28,43 @@ function shuffle(array) {
   }
 
 client.on('message', (message) => {
+
+    if(message.content.startsWith('#pickrandom')) {
+        if(message.content === '#pickrandom') {
+            message.channel.send('Please input a list of elements for me to work with.')
+        } else {
+            const allElements = message.content.slice(11).trim().replace(/\s+/g, ' ').split(' ')
+            const random = Math.floor(Math.random() * allElements.length)
+            const randomElement = allElements[random]
+            message.channel.send(randomElement + ' is the chosen one.')
+        }
+    }
+
     if(message.content.startsWith('#random')) {
         if(message.content === '#random') {
-            // guild.members.fetch()
-            //     .then(console.log)
             message.channel.send('Please input a list of players for me to work with.')
         } else {
             const allPlayers = message.content.slice(7).trim().replace(/\s+/g, ' ').split(' ')
             const shuffledPlayers = shuffle(allPlayers)
-            message.channel.send("Team 1: " + shuffledPlayers.slice(0,Math.floor(shuffledPlayers.length) / 2).join(' '))
-            message.channel.send("Team 2: " + shuffledPlayers.slice(Math.floor(shuffledPlayers.length) / 2).join(' '))
+            const embedOne = new MessageEmbed()
+            .addField('Team One', '1. Keefe\n2. Keefe\n3. Keefe\n4. Keefe\n5. Keefe')
+            .setThumbnail('https://via.placeholder.com/150')
+            message.channel.send(embedOne);
+
+            const embedTwo = new MessageEmbed()
+            .addField('Team One', '1. Keefe\n2. Keefe\n3. Keefe\n4. Keefe\n5. Keefe')
+            .setThumbnail('https://via.placeholder.com/150')
+            message.channel.send(embedTwo);
+
+            // message.channel.send("Team 1: " + shuffledPlayers.slice(0,Math.floor(shuffledPlayers.length) / 2).join(' '))
+            // message.channel.send("Team 2: " + shuffledPlayers.slice(Math.floor(shuffledPlayers.length) / 2).join(' '))
         }
     }
 })
 
 client.login(process.env.CLASH_BOT_TOKEN)
+
+/* 
+Blackpink
+TWICE
+*/
